83bc12bf8d2e79d23ba39db683868a2c9ee4e69d
Mayank17j <mayank17j@gmail.com>
2024-11-27 19:09:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Stock Portfolio 
+  Rajsi Traders is a small firm that has launched a platform where it will display a few stocks which is for working employees. In their platform, they have a few stocks mentioned which you can add to your portfolio and it will also generate the “Profit/Loss” scenario based on the market price and current stock price.
+  They have given this project to you to create the backend of the platform and help them generate the relevant prices.
+
+What is the Stock portfolio Platform?
+  The user can see 6 most relevant stocks on the platform.
+  To move ahead, they need to add at least 4 stocks in their portfolio first.
+  While adding the stocks, the user also needs to mention the quantity of the stocks they wish to purchase.
+  Once added, it will generate the Market Price and Bought At price automatically.
+
+Once you have added all the stocks, the user can see their “Stock Portfolio”
+  When the user clicks “View Returns” for each stock, they will be able to see the return for each stock they have added in their portfolio. Return = (marketPrice - boughtAt) * quantity
+  Now, the user will need to calculate the return percentage based on the returns they received which will be: returnPercentage = (returns / boughtAt) * 100
+  After calculating the Return Percentage, the user can now see the “Status” of their stock portfolio for each stock Profit or Loss
+  And at the end, the user can see their cumulative result of the portfolio
+    Total Returns
+    Total Growth
---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const cors = require('cors');
+const { resolve } = require('path');
+
+const app = express();
+const port = 3000;
+app.use(cors());
+
+app.use(express.static('static'));
+
+/*
+Endpoint 1: Calculate the Returns of the Stocks added
+API Call: <http://localhost:3000/calculate-returns?boughtAt=300&marketPrice=400&quantity=2>
+Expected Output: 200
+*/
+function calculateReturns(boughtAt, marketPrice, quantity) {
+  return (marketPrice - boughtAt) * quantity;
+}
+app.get('/calculate-returns', (req, res) => {
+  let boughtAt = parseFloat(req.query.boughtAt);
+  let marketPrice = parseFloat(req.query.marketPrice);
+  let quantity = parseFloat(req.query.quantity);
+  let result = calculateReturns(boughtAt, marketPrice, quantity);
+
+  res.send(result.toString());
+});
+
+/*
+Endpoint 2: Calculate the Total Returns
+API Call: <http://localhost:3000/total-returns?stock1=100&stock2=200&stock3=200&stock4=400>
+Expected Output: 900
+*/
+function totalReturns(stock1, stock2, stock3, stock4) {
+  return stock1 + stock2 + stock3 + stock4;
+}
+app.get('/total-returns', (req, res) => {
+  let stock1 = parseFloat(req.query.stock1);
+  let stock2 = parseFloat(req.query.stock2);
+  let stock3 = parseFloat(req.query.stock3);
+  let stock4 = parseFloat(req.query.stock4);
+  let result = totalReturns(stock1, stock2, stock3, stock4);
+
+  res.send(result.toString());
+});
+
+/*
+Endpoint 3: Calculate the Return Percentage
+API Call: <http://localhost:3000/calculate-return-percentage?boughtAt=400&returns=200>
+Expected Output: 50
+*/
+function calculateReturnPercentage(boughtAt, returns) {
+  return (returns * 100) / boughtAt;
+}
+app.get('/calculate-return-percentage', (req, res) => {
+  let boughtAt = parseFloat(req.query.boughtAt);
+  let returns = parseFloat(req.query.returns);
+  let result = calculateReturnPercentage(boughtAt, returns);
+
+  res.send(result.toString());
+});
+
+/*
+Endpoint 4: Calculate the Total Return Percentage
+API Call: <http://localhost:3000/total-return-percentage?stock1=10&stock2=20&stock3=20&stock4=40>
+Expected Output: 90
+*/
+function totalReturns(stock1, stock2, stock3, stock4) {
+  return stock1 + stock2 + stock3 + stock4;
+}
+app.get('/total-return-percentage', (req, res) => {
+  let stock1 = parseFloat(req.query.stock1);
+  let stock2 = parseFloat(req.query.stock2);
+  let stock3 = parseFloat(req.query.stock3);
+  let stock4 = parseFloat(req.query.stock4);
+
+  let result = totalReturnPercentage(stock1, stock2, stock3, stock4);
+  res.send(result.toString());
+});
+
+/*
+Endpoint 5: Identify the Status of Stocks based on their Return Value
+API Call: <http://localhost:3000/status?returnPercentage=90>
+Expected Output: profit
+*/
+function status(returnPercentage) {
+  if (returnPercentage === 0) return 'no profit, no loss';
+  else return returnPercentage < 0 ? 'loss' : 'profit';
+}
+app.get('/status', (req, res) => {
+  let returnPercentage = parseFloat(req.query.returnPercentage);
+  let result = status(returnPercentage);
+
+  res.send(result);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
